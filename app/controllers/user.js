4b761c8cf9a2ff72073fe6c6b9f787788eedd6b1
Koby Damari <kobyda@payoneer.com>
2016-11-30 20:36:46 +0200

    changes in templates

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,8 +1,41 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  callStatus: [
+    {
+      name: 'Just started'
+    },
+    {
+      name: 'A faw minutes ago'
+    },
+    {
+      name: 'Over 10 minutes'
+    },
+    {
+      name: 'Over 20 minutes'
+    },
+  ],
+  showStatusSelect:  Ember.computed('model.channel', function() {
+    return (this.get('model.channel') == '1' ||this.get('model.channel') == '4')
+    // body
+  }),
   actions: {
-    callForHelp() {
+    logOut(){
+      this.transitionToRoute('index' );
+    },
+    edit() {
+      if (this.get('model.userName') === 'dashboard') {
+        this.transitionToRoute('dashboard');
+      }
+      else if (this.get('model.isAdmin')) {
+        this.transitionToRoute('admin');
+      }
+      else if  (true) {
+        this.transitionToRoute('user' );
+      }
+
+    },
+    SubmitCall() {
       if (this.get('model.userName') === 'dashboard') {
         this.transitionToRoute('dashboard',this.get('model'));
       }
@@ -13,6 +46,13 @@ export default Ember.Controller.extend({
         this.transitionToRoute('user');
       }
 
+    },
+    openModal(){
+      this.set('modalShown', true)
+    },
+    closePromptDialog(){
+          this.set('modalShown',false)
     }
   }
+
 });
