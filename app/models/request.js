2062e43dd273bf43cfa097cf89bcd2792105e379
cuzi <tommer800@gmail.com>
2016-11-30 12:00:16 +0200

    Added models

---
+++ app/models/request.js
@@ -0,0 +1,15 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    c_id:        DS.attr('string'),
+    c_name:      DS.attr('string'),
+    c_country:   DS.attr('string'),
+    description: DS.attr('string'),
+    topic:       DS.attr('string'),
+    channel:     DS.attr('string'),
+    status:      DS.attr('string'),
+    req_time:    DS.attr('date'),
+    start_time:  DS.attr('date'),
+    end_time:    DS.attr('date'),
+    user:        DS.belongsTo('user')
+});
