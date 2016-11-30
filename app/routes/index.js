6f4908239bd2b3bbcf5276fcfd252d74502513c7
cuzi <tommer800@gmail.com>
2016-11-30 15:01:40 +0200

    Model fn added

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        let user = this.store.createRecord("user");
+        let invoiceSender = this.store.createRecord('request', {
+            user: user
+        });
+        return user;
+    },
+    registerListener: function(){
+
+    }.on('init')
+    
 });
