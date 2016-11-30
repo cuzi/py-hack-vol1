db1b01f62b16450aeec1a7e7a2091d581612557c
Koby Damari <kobyda@payoneer.com>
2016-11-30 15:02:31 +0200

    second commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions:{
+    login(){
+      console.log(this)
+      if(name2.toLowerCase() == 'admin'){
+        alert('ADMIN !');
+      }
+      else{
+          alert('im not admin')
+      }
+    }
+  }
+
+
 });
