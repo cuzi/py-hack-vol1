9044c3aceb2e8c2ee1a47acea89b12dd8883200b
cuzi <tommer800@gmail.com>
2016-12-01 04:55:13 +0200

    models fix

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-        return this.store.findAll('request');
+        //return this.store.findAll('request');
+        return this.store.query('request', {'status':  'pending'}).then((users)=> {
+           this.store.findAll('user');
+         return users.findBy('status', 'pending');
+       });
    }
 
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
   },
   model: function(params) {
 
-       return this.store.query('users', {'userName':  params.userName}).then(function(users) {
+       return this.store.query('users', {'userName':  params.userName}).then((users) =>{
            this.store.findAll('request')
          return users.findBy('userName', params.userName);
        });
