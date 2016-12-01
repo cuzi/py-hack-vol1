5cfb43bbab002d9fbb583f335c10de39189dd2ed
cuzi <tommer800@gmail.com>
2016-12-01 05:18:35 +0200

    buug fix

--- app/routes/user.js
+++ app/routes/user.js
@@ -6,7 +6,7 @@ export default Ember.Route.extend({
   },
   model: function(params) {
 
-       return this.store.query('users', {'userName':  params.userName}).then((users) =>{
+       return this.store.query('user', {'userName':  params.userName}).then((users) =>{
            this.store.findAll('request')
          return users.findBy('userName', params.userName);
        });
