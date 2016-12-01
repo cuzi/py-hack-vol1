aee28e4e97b158a27fd40dcaabddb2547756c44e
Koby Damari <kobyda@payoneer.com>
2016-12-01 02:01:59 +0200

    sadasdas

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  user: {
+    userName: ''
+  },
+  model: function(params) {
+
+       return this.store.query('user', {'userName':  params.userName}).then((users) => {
+           this.store.findAll('request');
+       });
+   }
 });
